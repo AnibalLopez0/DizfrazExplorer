c8476944a2e6dacf247e175e24e136239c787b38
dulcecand7777 <dulcecandy291@gmail.com>
2024-05-02 10:56:17 -0600

    Insertar empleado y proveedor
    Creación de las funciones para insertar empelado y proveedor en la base de datos, además de validar que no se dejen campos vacios

--- Disfraz/screens/ProveedorRegister.js
+++ Disfraz/screens/ProveedorRegister.js
@@ -1,10 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { View, Text, TextInput, SafeAreaView, ImageBackground, Image, TouchableOpacity,
 ScrollView } from 'react-native';
 import {Subtitle2, Input, ButtonsNormal, ButtonsNormal2, Buttons} from './Styles';
 import Icons from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
 
 const ProveedorRegister = () =>  {
+
+    const [nombre, setNombre] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [ubicacion, setUbicacion] = useState('');
+
+    const enviarDatos = async () => {
+        try{
+            const url = "https://snek22.000webhostapp.com/insertarproveedor.php";
+            const proveedor = 
+            {
+                nombre: nombre,
+                correo: correo,
+                telefono: telefono,
+                ubicacion: ubicacion
+            }
+            const response = await axios.post(url, { proveedores: [proveedor] });
+
+            console.log('Proveedor reagistrado correctamente:', response.data);
+            alert(nombre +' agregado correctamente');
+            navigation.reset({
+                index: 0,
+                routes: [{ name: 'Inventory' }],
+            })
+
+        }catch(error){
+            console.error('Error al enviar los datos:', error, nombre, correo, telefono, ubicacion);
+            alert('Error al registrar empleado');
+        }
+    };
+
+    const camposVacios = () => {
+        if(!nombre || !telefono || !correo || !ubicacion){
+            alert("Porfavor completa todos los campos");
+            return false;
+        }
+        return true;
+    }
+
     return(
         <SafeAreaView>
             <ImageBackground
@@ -17,25 +57,38 @@ const ProveedorRegister = () =>  {
                     <Text style={[Subtitle2, {marginTop:20, marginLeft:10}]}>
                         NOMBRE DEL PROVEEDOR</Text>
                     <TextInput style={Input}
+                    value={nombre}
+                    onChangeText={setNombre}
                     placeholder='Nombre del proveedor'></TextInput>
                     <Text style={[Subtitle2, {marginTop:20, marginLeft:10}]}>
                         CORREO</Text>
                     <TextInput style={Input}
+                    value={correo}
+                    onChangeText={setCorreo}
                     placeholder='Correo'></TextInput>
                     <Text style={[Subtitle2, {marginTop:20, marginLeft:10}]}>
                         TELEFONO</Text>
                     <TextInput style={Input}
+                    value={telefono}
+                    onChangeText={setTelefono}
                     placeholder='Telefono'></TextInput>
                     <Text style={[Subtitle2, {marginTop:20, marginLeft:10}]}>
                         UBICACION</Text>
                     <TextInput style={Input}
+                    value={ubicacion}
+                    onChangeText={setUbicacion}
                     placeholder='Ubicacion'></TextInput>
                 </View>
                 <View style={{marginTop: '5%'}}>
                 <TouchableOpacity style={[ButtonsNormal, {marginLeft: 25,}]}>
                     <Text style={Buttons}>CANCELAR</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={[ButtonsNormal2, {marginTop:-35, marginLeft:225}]}>
+                    <TouchableOpacity style={[ButtonsNormal2, {marginTop:-35, marginLeft:225}]}
+                    onPress={() => {
+                        if (camposVacios()) {
+                          enviarDatos();
+                        }
+                      }}>
                     <Text style={Buttons}>TERMINAR</Text>
                     </TouchableOpacity>
                 </View>
--- Disfraz/screens/Register.js
+++ Disfraz/screens/Register.js
@@ -2,17 +2,52 @@ import { Text, View, ImageBackground, SafeAreaView, TextInput, TouchableOpacity 
 import React, { Component, useState } from 'react'
 import {Background, ButtonsLogin, ButtonsLoginText, Input, SubtitleInput} from './Styles';
 import Icons from 'react-native-vector-icons/Ionicons';
-
+import axios from 'axios';
 
 const Register = ({navigation}) => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [password, setPassword] = useState('');
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const enviarDatos = () => {
+  const enviarDatos = async () => {
+    try{
+      const url = "https://snek22.000webhostapp.com/insertarusuario.php";
 
+      const empleado = 
+      {
+        nombre: nombre,
+        cargo: cargo,
+        correo: correo,
+        password: password,
+      }
+      const response = await axios.post(url, { empleados: [empleado] });
+
+      console.log('Empleado reagistrado correctamente:', response.data);
+      alert(nombre +' agregado correctamente');
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Login' }],
+      })
+
+    } catch(error){
+      console.error('Error al enviar los datos:', error, nombre, correo, cargo, password);
+      alert('Error al registrar empleado');
+    }
+  }
+
+  const camposVacios =  () => {
+    if(!nombre || !correo || !cargo || !password){
+      alert("Por favor completa todos los campos");
+      return false;
+    }
+    return true;
   }
 
   return (
@@ -26,34 +61,48 @@ const Register = ({navigation}) => {
               <Text style={SubtitleInput}>
                 NOMBRE
               </Text>
-              <TextInput style={Input}></TextInput>
+              <TextInput style={Input}
+              value={nombre}
+              onChangeText={setNombre}></TextInput>
             </View>
             <View style={{marginTop:15}}>
               <Text style={SubtitleInput}>
                 CORREO
               </Text>
-              <TextInput style={Input}></TextInput>
+              <TextInput style={Input}
+              value={correo}
+              onChangeText={setCorreo}></TextInput>
             </View>
             <View style={{marginTop:15, marginBottom:15}}>
               <Text style={SubtitleInput}>
-                USUARIO
+                CARGO
               </Text>
-              <TextInput style={Input}></TextInput>
+              <TextInput style={Input}
+              value={cargo}
+              onChangeText={setCargo}></TextInput>
             </View>
             <View>
             <Text style={SubtitleInput}>CONTRASEÑA</Text>
-            <TextInput style={Input} secureTextEntry={!showPassword}></TextInput>
+            <TextInput style={Input} secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}></TextInput>
             <TouchableOpacity style={{marginTop:-40, marginLeft:255}} onPress={togglePasswordVisibility}>
             <Icons name={showPassword ? "eye" : "eye-off"} color={"black"} size={40}/>
             </TouchableOpacity>
           </View>
           </View>
           <View style={{flex: 1, alignItems:'center', marginTop:250}}>
-            <TouchableOpacity style={ButtonsLogin}>
+            <TouchableOpacity style={ButtonsLogin}
+            onPress={() => {
+              if (camposVacios()) {
+                enviarDatos();
+              }
+            }}>
               <Text style={[ButtonsLoginText, {marginLeft:100}]}>REGISTRARSE</Text>
             </TouchableOpacity>
             <TouchableOpacity
-            onPress={() => navigation.navigate('Login')}>
+            onPress={() => navigation.navigate('Login')}
+            >
               <Text 
               style={{fontSize: 16, 
               fontWeight:'bold', 
